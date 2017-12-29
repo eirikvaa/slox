58817e4c8077201b076512fec1ee717d1c9b33e4
Eirik Vale Aase <e.v.aase@gmail.com>
2017-12-29 17:00:13 +0100

    Initial Commit

---
+++ slox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slox
+//
+//  Created by Eirik Vale Aase on 29.12.2017.
+//  Copyright © 2017 Eirik Vale Aase. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
